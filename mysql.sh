302f6db26b51140f6be29f33b9f7c845600d4d5f
Ganesh281 <varagantiganesh69@gmail.com>
2024-09-02 16:57:34 +0530

    expense-shell

---
+++ mysql.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOGFILE="$LOGS_FOLDER/$SCRIPTNAME-$TIMESTAMP.log"
+
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+#echo "User ID is: $USERID"
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root privileges $N" | tee -a $LOGFILE
+        exit 1
+    fi
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 is...$R FAILED $N" | tee -a $LOGFILE
+        exit 1
+    else
+        echo -e "$2 is...$G SUCCESS $N" | tee -a $LOGFILE
+    fi
+}
+
+echo "Script satrts executeing at : $(date)" | tee -a $LOGFILE
+
+CHECK_ROOT
+
+dnf install mysql-server -y
+VALIDATE $? "Installing MySQL Server"
+
+systemctl enable mysqld
+VALIDATE $? "enabled MySQL Server"
+
+systemctl start mysqld
+VALIDATE $? "started MySQL server"
+
+mysql_secure_installation --set-root-pass ExpenseApp@1
+VALIDATE $? "Setting Up root password"
