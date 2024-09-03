40cc41b7d2803d5400a1d5c8f9526e9c00395e46
Ganesh281 <varagantiganesh69@gmail.com>
2024-09-03 23:17:35 +0530

    expense-shell

--- backend.sh
+++ backend.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 LOGS_FOLDER="/var/log/expense"
-SCRIPTNAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOGFILE="$LOGS_FOLDER/$SCRIPTNAME-$TIMESTAMP.log"
-
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
 USERID=$(id -u)
-#echo "User ID is: $USERID"
-
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
@@ -18,33 +15,33 @@ Y="\e[33m"
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
     then
-        echo -e "$R Please run this script with root privileges $N" | tee -a $LOGFILE
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
         exit 1
     fi
 }
 
 VALIDATE(){
     if [ $1 -ne 0 ]
-    then 
-        echo -e "$2 is...$R FAILED $N" | tee -a $LOGFILE
+    then
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
         exit 1
     else
-        echo -e "$2 is...$G SUCCESS $N" | tee -a $LOGFILE
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
 
-echo "Script satrts executeing at : $(date)" | tee -a $LOGFILE
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
 dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "Disable default js"
+VALIDATE $? "Disable default nodejs"
 
 dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "enabling the nodejs"
+VALIDATE $? "Enable nodejs:20"
 
 dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "installing nodejs"
+VALIDATE $? "Install nodejs"
 
 id expense &>>$LOG_FILE
 if [ $? -ne 0 ]
